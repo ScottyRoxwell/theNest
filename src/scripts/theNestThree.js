370d0c714f4b7b744b6ee3415a805ce61ddb913c
ScottyRoxwell <scottyroxwell@gmail.com>
2019-10-30 04:03:52 -0700

    Lighting is coming along. Time to go to bed.

--- src/scripts/theNestThree.js
+++ src/scripts/theNestThree.js
@@ -2,6 +2,7 @@ import {THREE} from '../vendor';
 // import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader";
 import {GodRaysEffect, RenderPass, EffectPass, EffectComposer} from 'postprocessing';
 import noise from './utils/perlinNoise';
+import { SceneUtils } from 'three';
 
 
 const GLTFLoader = require('./gltfloader');
@@ -14,7 +15,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(80,width/height,1,1000);
 // scene.add(camera)
 
-camera.position.z = 590;
+camera.position.z = 570;
 camera.position.y = 0;
 camera.lookAt(0,0,0);
 
@@ -22,21 +23,6 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(width,height);
 const canvas = document.getElementById('canvas');
 canvas.appendChild(renderer.domElement);
-
-// THE NEST
-// const loader = new THREE.TextureLoader();
-// let theNestMat = loader.load('../images/nestFron3.png');
-// theNestMat.minFilter = THREE.LinearFilter;
-// // theNestMat.flipY = false;
-// let nestMaterial = new THREE.MeshBasicMaterial({map:theNestMat, transparent: true, side: THREE.FrontSide});
-// let theNestPlane = new THREE.PlaneBufferGeometry(1,1);
-// let theNest = new THREE.Mesh(theNestPlane,nestMaterial);
-// theNest.scale.x = 1920;
-// theNest.scale.y = 1280;
-// theNest.position.y = -100;
-// theNest.position.z = -10
-// // theNest.rotateY(Math.PI)
-// scene.add(theNest)
 
 let mouseX;
 let mouseY;
@@ -49,13 +35,9 @@ function moveNest(e){
   return mouseX, mouseY;
 }
 
-
-
-
-
 // THE NEST
 const loader = new THREE.GLTFLoader();
-loader.load('../objects/theNest19.glb', (gltf) => {
+loader.load('../objects/theNest20.glb', (gltf) => {
   let nest = gltf.scene.children[0];
 
   nest.rotation.x = Math.PI/2
@@ -70,13 +52,9 @@ loader.load('../objects/theNest19.glb', (gltf) => {
   renderer.render(scene,camera)
 })
 
-const light = new THREE.AmbientLight(0xffffff, 1);
-scene.add(light)
-console.log(scene)
-
 // MOON
 const moonGeo = new THREE.CircleGeometry(50,30);
-const moonMat = new THREE.MeshBasicMaterial({color: 0xaaaaaa});
+const moonMat = new THREE.MeshBasicMaterial({color: 0x777788});
 const circle = new THREE.Mesh( moonGeo, moonMat);
 circle.position.set(250, 182, -20);
 scene.add(circle)
@@ -89,12 +67,7 @@ let godraysEffect = new GodRaysEffect(camera, circle,{
   weight: .2,
   samples: 320,
   blur: false
-})
-
-// FOG
-// const fog = new THREE.Fog(0xffffff);
-// fog.position.z = 0
-// scene.add(fog);
+});
 
 let renderPass = new RenderPass(scene,camera);
 let effectPass = new EffectPass(camera,godraysEffect);
@@ -103,14 +76,6 @@ effectPass.renderToScreen = true;
 let composer = new EffectComposer(renderer);
 composer.addPass(renderPass);
 composer.addPass(effectPass);
-
-// const light = new THREE.DirectionalLight( 0xffffff);
-// light.position.set(-340,-95,5)
-// light.lookAt(-350,-30,-5)
-// scene.add( light );
-
-
-
 
 // STARS
 const particle = {
@@ -140,15 +105,69 @@ const particle = {
   }
 }
 
-
-
-
 // NEST LIGHTS
-const light1 = new THREE.PointLight(0xffffff,200,0,.2);
-
-light1.position.z = -9;
+const lightColor = new THREE.Color(0xffffaa)
+const penumbra = .4;
+const light1 = new THREE.SpotLight(lightColor,2);
+light1.penumbra = penumbra;
+light1.position.x = 288;
+light1.position.y = -96;
+light1.position.z = -1;
+light1.target = new THREE.Object3D();
+light1.target.position.set(light1.position.x+5,light1.position.y-50, light1.position.z);
+scene.add(light1.target);
 scene.add(light1);
 
+const light2 = new THREE.SpotLight(lightColor,2);
+light2.penumbra = penumbra;
+light2.position.x = 525;
+light2.position.y = -44;
+light2.position.z = -1;
+light2.target = new THREE.Object3D();
+light2.target.position.set(light2.position.x+7,light2.position.y-50, light2.position.z);
+scene.add(light2.target);
+scene.add(light2);
+
+const light3 = new THREE.SpotLight(lightColor,2);
+light3.penumbra = penumbra;
+light3.position.x = 828;
+light3.position.y = 32;
+light3.position.z = -1;
+light3.target = new THREE.Object3D();
+light3.target.position.set(light3.position.x+9,light3.position.y-50, light3.position.z);
+scene.add(light3.target);
+scene.add(light3);
+
+// AMBIENT LIGHT
+const light = new THREE.AmbientLight(0xffeeee, .3);
+scene.add(light)
+
+// DIRECTIONAL LIGHT
+// const directional = new THREE.DirectionalLight(0xffffff,2);
+// directional.position.set(200,0,300)
+// directional.target = new THREE.Object3D();
+// directional.target.position.set(-100,0,-10)
+// scene.add(directional.target)
+// scene.add(directional);
+
+// HEADLIGHTS
+// const headlight1 = new THREE.SpotLight(0xbbbbff,1.3);
+// headlight1.penumbra = .4
+// headlight1.angle = Math.PI/6
+// headlight1.position.set(0,-300,800);
+// headlight1.target = new THREE.Object3D();
+// scene.add(headlight1.target);
+// scene.add(headlight1);
+
+// const headlight2 = new THREE.SpotLight(0xbbbbff,1.3);
+// headlight2.penumbra = .4
+// headlight2.angle = Math.PI/6
+// headlight2.position.set(600,-300,800);
+// headlight2.target = new THREE.Object3D();
+// scene.add(headlight2.target);
+// scene.add(headlight2);
+
+// SHOOTING STARS
 const shootingStar = {
   scale: 1,
 
@@ -170,13 +189,24 @@ function createSky(amount){
 createSky(1000)
 
 let delta = 0;
+let pdelta = 0;
+let p,q,r;
 const animate = function () {
   requestAnimationFrame( animate );
   delta = 0.00011;
 
-  light1.position.x = mouseX;
-  light1.position.y = mouseY;
-  // theNest.position.y -= delta*1000
+  // LIGHT FLICKER
+  pdelta += 0.07
+  p = noise(pdelta);
+  q = noise(pdelta*.7);
+  r = noise(pdelta*.5);
+  light1.intensity = THREE.Math.mapLinear(p,0,1,4.5,6);
+  light2.intensity = THREE.Math.mapLinear(q,0,1,4.5,7);
+  light3.intensity = THREE.Math.mapLinear(r,0,1,4.5,8);
+
+  // headlight1.target.position.set(mouseX,mouseY,-10);
+  // headlight2.target.position.set(mouseX+400,mouseY,-10);
+
   let moonMaxHeight = 690;
   circle.position.y += (circle.position.y >= moonMaxHeight-200) ? 0 : (moonMaxHeight-circle.position.y)*.0002;
 
