784a9485b9bb24088fee4f517a20defe0b1c390e
ScottyRoxwell <scottyroxwell@gmail.com>
2019-11-26 12:25:30 -0800

    Playing with depthFunc on nest material.

--- src/scripts/theNestThree.js
+++ src/scripts/theNestThree.js
@@ -71,6 +71,7 @@ function init(){
     nest.scale.set(32,1,32);
     nest.position.y = -200;
     nest.position.z = -10.0;
+    nest.material.depthFunc = 2;
 
     // Masking Layer
     let maskMap = mask.material.map;
